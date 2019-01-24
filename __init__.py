c30a1d6d715c4930c0f1e25a90dbb8c256909a23
Jeff Rowberg <jeff@rowberg.net>
2019-01-23 21:35:19 -0500

    Add documentation to module init files

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
+"""
+This module provides protocol and packet definitions for communicating with
+Bluegiga (now Silicon Labs) modules that implement the BGAPI protocol. This set
+of devices includes the BLE112, BLED112, BLE113, and BLE121LR Bluetooth Low
+Energy modules, WF121 and WGM110 Wifi modules, and BT121 Bluetooth Smart Ready
+(dual-mode) module.
+"""
+
 # .py files
 from .protocol import *
